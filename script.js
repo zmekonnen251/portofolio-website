f060d5a7af987766fc8ae93848389102b3b8d1be
Zelalem <mekonnen.zelalem251@gmail.com>
2022-01-25 08:56:47 +0100

    fix overfly of mobile menu

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const mobileMenu = document.querySelector('.nav img');
-const header = document.querySelector('header');
-const mobileMenuList = ['Portfolio', 'About', 'Contact'];
+const mobileMenu = document.querySelector(".nav img");
+const header = document.querySelector("header");
+const mobileMenuList = ["Portfolio", "About", "Contact"];
 
 const displayMenu = () => {
-  document.querySelector('.nav').classList.add('dn');
-  const nav = document.createElement('nav');
-  nav.className = 'mobile-nav-container';
-  const xIcon = document.createElement('img');
-  xIcon.src = './img/x-Icon.png';
-  xIcon.alt = 'x icon';
-  const ul = document.createElement('ul');
-  ul.className = 'mobile_nav';
+  document.querySelector(".nav").classList.add("dn");
+  document.querySelector("body").classList.add("no-scroll");
+  header.classList.add("h-100");
+  const nav = document.createElement("nav");
+  nav.className = "mobile-nav-container";
+  const xIcon = document.createElement("img");
+  xIcon.src = "./img/x-Icon.png";
+  xIcon.alt = "x icon";
+  const ul = document.createElement("ul");
+  ul.className = "mobile_nav";
   for (let i = 0; i < 3; i += 1) {
-    const element = document.createElement('li');
-    element.className = 'mobile-item-list';
-    const anchorTag = document.createElement('a');
+    const element = document.createElement("li");
+    element.className = "mobile-item-list";
+    const anchorTag = document.createElement("a");
     anchorTag.innerText = mobileMenuList[i];
     anchorTag.href = `#${mobileMenuList[i].toLowerCase()}`;
-    anchorTag.className = 'mobile_menu_links';
+    anchorTag.className = "mobile_menu_links";
     element.appendChild(anchorTag);
     ul.appendChild(element);
   }
@@ -28,14 +30,16 @@ const displayMenu = () => {
 
 const resetDefault = (event) => {
   if (
-    (event.target
-      && event.target.parentNode.className === 'mobile-nav-container')
-    || (event.target && event.target.className === 'mobile_menu_links')
+    (event.target &&
+      event.target.parentNode.className === "mobile-nav-container") ||
+    (event.target && event.target.className === "mobile_menu_links")
   ) {
-    document.querySelector('.mobile-nav-container').remove();
-    document.querySelector('.nav').classList.remove('dn');
+    document.querySelector(".mobile-nav-container").remove();
+    document.querySelector(".nav").classList.remove("dn");
+    document.querySelector("body").classList.remove("no-scroll");
+    header.classList.remove("h-100");
   }
 };
-mobileMenu.addEventListener('click', displayMenu);
+mobileMenu.addEventListener("click", displayMenu);
 
-document.addEventListener('click', (event) => resetDefault(event));
+document.addEventListener("click", (event) => resetDefault(event));
